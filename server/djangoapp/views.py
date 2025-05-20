d4aaca3ad87434535820e1bf16e8fad17ca251b4
tiaracalistaka <tiaracalistaka@gmail.com>
2025-05-20 03:41:22 -0400

    created the CarModel and CarMake models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,14 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
+from .models import CarMake, CarModel
+from .populate import initiate
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -63,3 +65,13 @@ def login_user(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
